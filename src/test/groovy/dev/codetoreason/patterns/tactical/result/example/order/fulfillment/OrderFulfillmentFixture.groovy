39f98c64b53c0b3606e861f72602179922493366
maleksandrowicz93 <mateusz.aleksandrowicz93@gmail.com>
2025-05-14 11:53:29 +0200

    simplify order fulfillment test api

--- src/test/groovy/dev/codetoreason/patterns/tactical/result/example/order/fulfillment/OrderFulfillmentFixture.groovy
+++ src/test/groovy/dev/codetoreason/patterns/tactical/result/example/order/fulfillment/OrderFulfillmentFixture.groovy
@@ -4,9 +4,18 @@ import dev.codetoreason.patterns.tactical.infra.event.Event
 import dev.codetoreason.patterns.tactical.infra.event.EventPublisher
 import spock.lang.Specification
 
+import static dev.codetoreason.patterns.tactical.result.example.order.fulfillment.ProductType.STANDARD
+
 class OrderFulfillmentFixture extends Specification {
 
+    static final OrderId ORDER_ID = new OrderId("ORDER-1")
+    static final Product PRODUCT = Product.builder()
+                                          .id(new ProductId("PROD-1"))
+                                          .name("AnyName")
+                                          .type(STANDARD)
+                                          .build()
     static final ShipmentId SHIPMENT_ID = new ShipmentId("shipment-123")
+    static final String DEFAULT_REGION_NAME = "REGION-1"
 
     private final def shippingServiceStub = Stub(ShippingService) {
         dispatch(_ as OrderId, _ as WarehouseId) >> SHIPMENT_ID
@@ -16,22 +25,20 @@ class OrderFulfillmentFixture extends Specification {
     private final def warehouseRepository = new InMemoryWarehouseRepository()
     private final def factory = new OrderFulfillmentFacadeFactory()
 
-    static OrderFulfillmentFacade freshFacade() {
-        new OrderFulfillmentFixture().buildFacade()
-    }
-
     static OrderFulfillmentFixture create() {
         new OrderFulfillmentFixture()
     }
 
-    OrderFulfillmentFixture withOrder(Order order) {
-        orderRepository.save(order)
-        this
+    static OrderFulfillmentFacade freshFacade() {
+        new OrderFulfillmentFixture().buildFacade()
     }
 
-    OrderFulfillmentFixture withWarehouse(Warehouse warehouse) {
-        warehouseRepository.save(warehouse)
-        this
+    WarehouseBuilder withWarehouse(WarehouseId id) {
+        return new WarehouseBuilder(this, id)
+    }
+
+    OrderBuilder withOrder() {
+        return new OrderBuilder(this)
     }
 
     OrderFulfillmentFacade buildFacade() {
@@ -45,5 +52,77 @@ class OrderFulfillmentFixture extends Specification {
 
     void verifyEventPublished(Event event) {
         1 * eventPublisherMock.publish(event)
+    }
+
+    class WarehouseBuilder {
+        private final OrderFulfillmentFixture fixture
+        private final WarehouseId id
+        private String region = DEFAULT_REGION_NAME
+        private Set<ProductType> supportedTypes = Set.of(PRODUCT.type())
+        private int quantity = 10
+
+        WarehouseBuilder(OrderFulfillmentFixture fixture, WarehouseId id) {
+            this.fixture = fixture
+            this.id = id
+        }
+
+        WarehouseBuilder assignedToRegion(String region) {
+            this.region = region
+            this
+        }
+
+        WarehouseBuilder supporting(ProductType... types) {
+            this.supportedTypes = Set.of(types)
+            this
+        }
+
+        WarehouseBuilder stockingQuantity(int quantity) {
+            this.quantity = quantity
+            this
+        }
+
+        OrderFulfillmentFixture and() {
+            fixture.warehouseRepository.save(
+                    Warehouse.builder()
+                             .id(id)
+                             .region(region)
+                             .supportedTypes(supportedTypes)
+                             .stockLevels(Map.of(PRODUCT.id(), quantity))
+                             .build()
+            )
+            fixture
+        }
+    }
+
+    class OrderBuilder {
+        private final OrderFulfillmentFixture fixture
+        private int quantity = 5
+        private String region = DEFAULT_REGION_NAME
+
+        OrderBuilder(OrderFulfillmentFixture fixture) {
+            this.fixture = fixture
+        }
+
+        OrderBuilder ofQuantity(int quantity) {
+            this.quantity = quantity
+            this
+        }
+
+        OrderBuilder toRegion(String region) {
+            this.region = region
+            this
+        }
+
+        OrderFulfillmentFixture and() {
+            fixture.orderRepository.save(
+                    Order.builder()
+                         .id(ORDER_ID)
+                         .product(PRODUCT)
+                         .quantity(quantity)
+                         .destinationRegion(region)
+                         .build()
+            )
+            fixture
+        }
     }
 }
