1f3ba6b3255109d131b8d88470960d79f85d350a
maleksandrowicz93 <mateusz.aleksandrowicz93@gmail.com>
2025-06-10 18:15:06 +0200

    add ProfessorEmploymentFlowSpec with test cases for successful employment scenarios

--- src/test/groovy/dev/codetoreason/patterns/tactical/rule/example/university/professor/employment/ProfessorEmploymentFlowSpec.groovy
+++ src/test/groovy/dev/codetoreason/patterns/tactical/rule/example/university/professor/employment/ProfessorEmploymentFlowSpec.groovy
@@ -3,9 +3,10 @@ package dev.codetoreason.patterns.tactical.rule.example.university.professor.emp
 import spock.lang.Specification
 
 import static dev.codetoreason.patterns.tactical.rule.example.university.professor.employment.ProfessorEmploymentFixture.IT_FACULTY_ID
+import static dev.codetoreason.patterns.tactical.rule.example.university.professor.employment.ProfessorEmploymentFixture.ZUCK
 import static dev.codetoreason.patterns.tactical.rule.example.university.professor.employment.ProfessorEmploymentFixture.applicationWith
 
-class ProfessorEmploymentFailureSpec extends Specification {
+class ProfessorEmploymentFlowSpec extends Specification {
 
     def "should return empty when rules fail"() {
         given:
@@ -65,5 +66,29 @@ class ProfessorEmploymentFailureSpec extends Specification {
         then:
             def ex = thrown(NoSuchElementException)
             ex.message.contains("Entity with ID $IT_FACULTY_ID not found")
+    }
+
+    def "should employ professor when all rules pass"() {
+        given:
+            def fixture = ProfessorEmploymentFixture.create()
+                                                    .withRulesExaminedSuccessfully()
+                                                    .withSuccessfulEmploymentOperation()
+                                                    .withFacultyContainingFieldsOfStudies("cs", "ai", "math")
+            def facade = fixture.buildFacade()
+
+        when:
+            def result = facade.employProfessor(
+                    applicationWith {
+                        yearsOfExperience(10)
+                        fieldsOfStudies("cs", "ai", "math")
+                    }
+            )
+
+        then:
+            result.isPresent()
+            with(fixture.getProfessorById(result.get())) {
+                name() == ZUCK
+                fieldsOfStudies() == FieldsOfStudies.of("cs", "ai", "math")
+            }
     }
 }
